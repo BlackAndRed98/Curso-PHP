ab4b4947eb85c087db71779e1c863648a36328c2
BlackAndRed.eth <fnftrapstudio@gmail.com>
2022-12-14 15:51:45 +0100

    leccion 9

--- index.php
+++ index.php
@@ -4,30 +4,124 @@
         <meta charset="UTF-8">
         <title>CURSO BASICO PHP</title>
     </head>
-     <body>
+    <!--En este curso de ejercicios completados veremos todo lo basico sobre el lenguaje PHP 
+    con un servidor abierto desde wampserver y todos los serviciod conectados , En la parte Style crearemos un disenyo basico
+    para que asi nuestro curso se vea mas agradable, TODOS LOS EJERCICIOS YA ESTAN RESUELTOS.
+    Este documento explica y ensenya el codigo los resultados se ven en la pagina anadiendo LOCAL SERVER/CursoPHP en
+    la barra del navegador (siempre y cuando nuestro servidor este abierto).
 
-     <style>
+    Conocimientos basicos a tener en cuenta 
+    -- Seria medio obligatorio entender y saber lenguajes como HTML y CSS3.
+    -- Leer codigo / abrir un servidor en WampServer con todos los servicios activados.
+    -- Saber Ingles basico
+    -- Crear carpetas y rediccionar documentos (informatica basica)
+    
+    Mi nombre es FNF y este es el curso escrito de PHP siguiendo de guia el canal de Youtube de (PILDORASINFORMATICAS CURSO DE PHP Y MYSQL).
+    Gracias.
+   
+    1.Empezaremos creando un documento HTML en el cual crearemos nuestro Codigo.
+    2.A continuacion sabiendo un poco de Css pondremos colores de fondo y disenyo.
+
+   -->
+    <style>
       /*.Resaltar forma parte de la LECCION 6 */
       .resaltar {
         color: red;
         font-weight: bold;
       }
+      *{
+        text-align: center;
+        margin-left: 5px;
+        margin-right: 5px;
+        color: azure;
+        background-color: black;
+        
+      }
+      
 
+      h2{
+        color:orangered;
+        border-style: double;
+        border-color: blue;
+        border-radius: 10%;
+       
+      }
+
+      .resaltarAzul{
+        color: blue;
+        font-weight: bold;
+      }
+      
+      h1 {
+        text-align: center;
+      }
+
+     form{
+        border-style: ridge;
+        display: grid;
+        
+        height: 20vh;
+        display: flex;
+        justify-content: center;
+        color: aliceblue;
+        flex-direction: row;
+        flex-wrap: wrap;
+        align-items: center;  
+       
+      }
+
+      .boton{
+        background-color: black;
+        color: red;
+        font-size: larger;
+        border-style: ridge;
+        border-color: aliceblue;
+        font-family:'Times New Roman', Times, serif;
+        width: 150px;
+        
+        
+      }
+      .no_validado {
+        font-size: 18px;
+        color: red;
+        font-weight: bold;
+      }
+
+      .validado{
+        font-size: 18px;
+        color: green;
+        font-weight: bold;
+      }
+
+      
+      
+      
+
+    
 
      </style>
+     <body>
+      <h1>Curso PHP</h1>
+     
+
+      
+     </form>
+    
+
+    <!--A partir de aqui iremos separando los codigos PHP por Lecciones -->
 
    
         <?php
                /*Leccion 1/SINTAXIS/PRINT aqui vemos algunos ejemplos de etiquetas de abertura y cierre + algunos print con texto
                 incluyendo varias etiquetas <BR> para aplicar unos cuantos saltos de linea*/
-               echo "LECCION 1 <BR>";
+               echo "<h2>LECCION 1 / hacer prints con (echo)</h2> <BR>";
                print "Bienvenidos al curso de PHP<br>";
-               print "En este documento comenzaremos a conocer PHP <br> <br> <br>";    
+               print "En este documento comenzaremos a conocer PHP. <br> <br> <br> <h3>NOTA: Todos los detalles de los ejercicios y explicaciones estan en el documento php en comentarios.</h3><br> <br> <br>";    
         ?>
 
         <?php
                //Leccion 2/VARIABLES/CONCADENADORES Aqui declaramos algunas variables y las concadenamos usando un (.)      
-               echo "LECCION 2 <BR>";
+               echo "<h2>LECCION 2 sintaxis basica</h2> <BR>";
                $nombre = "FNF";
                $edad = 24;
 
@@ -41,7 +135,7 @@
 
         <?php
               /*LECCION 3/FUNCIONES declaramos 2 echos para que se vean en nuestra pagina*/
-              echo "LECCION 3 <BR>";
+              echo "<h2>LECCION 3 Primera Funcion</h2> <BR>";
               echo "Este es el primer mensaje<br>";
               /*declaramos una funcion que al ser llamada pondra un echo*/
               dameDatos();
@@ -63,13 +157,13 @@
               /*LECCION 4/GLOBAL/LOCAL/(SUPER GLOBAL lo daremos mas tarde)//
               En este ejercicio hemos declarado una variable $nombre y despues la hemos intentado re-asignar. 
               Daba un error ya que PHP creeia que nos estabamos refiriendo a una variable local\diferente a la nuestra.*/
-              echo "LECCION 4 <BR>";
+              echo "<h2>LECCION 4 GLOBAL / LOCAL / SUPERGLOBAL</h2> <BR>";
 
               $nombre = "BlackandRed";
               function dameNombre(){
               //Aqui arreglamos el error diciendole a PHP que nuestra variable $nombre es GLOBAL asi que empezara a re-asignar los nombres declarados.
               global $nombre;
-              $nombre = "Fran <br><br><br>";
+              $nombre = "FNF <br><br><br>";
             
             }
               dameNombre();
@@ -82,7 +176,7 @@
 
              /*LECCION 5/VARIABLES ESTATICAS/ Si no ponemos STATIC dentro de la funcion su valor se destruye al terminar la misma,
               poniendo static hacemos que su vcalor permanezca y ponamos usar el operador ++ para asi ir incrementando en 1 su valor cada vez que la llamemos. */
-              echo "LECCION 5 <BR>";
+              echo "<h2>LECCION 5 Variables Estaticas</h2> <BR>";
              function incrementaVariable(){
 
               static $contador = 0;
@@ -97,14 +191,15 @@
              incrementaVariable();
               
              echo "<br><br>";
+             /*CADA VEZ QUE LLAMEMOS A ESTA FUNCION (incrementarVariable) haremos que en nuestra pagina se vaya anyadiendo otro numero mas en orden incrementativo*/
         ?>
         
       <?php
          //LECCION 6/STRINGS
-         echo "LECCION 6 <BR>";
+         echo "<h2>LECCION 6 Strings</h2><BR>";
          //en la linea siguiente vemos un echo con un parrafo clase resaltar (que hemos creado al principio del body entre etquiteas <style>)
          //haciendo que este parrafo nuevo tenga un estilo definifo (resaltar en rojo)
-         echo"<p class='resaltar'>Esto es un ejemplo de frase</p>";
+         echo"<p class='resaltarAzul'>Esto es un ejemplo de frase</p>";
            
          /*Ahora creamos variables para hacer unas comparaciones con (strcm) & (strcasecmp).
          strcmp -> Compara los valores (string/letras) teniendo en cuenta las Mayusculas y Minusculas.
@@ -114,14 +209,16 @@
          $variable2 = "CASA";
          $resultado = strcasecmp($variable1,$variable2);
           if($resultado <= 0) {
-            echo "Los strings son iguales ya que el valor es $resultado";
+            echo "Los strings son iguales ya que el valor es $resultado <br><br>";
           }else{
-            echo "Los strings no son iguales ya que el resultado es $resultado";
+            echo "Los strings no son iguales ya que el resultado es $resultado <br> <br> <br> <br>";
           }
          ?>
+         
 
          <?php
          //Leccion 7/Operadores.
+         echo ("<h2>LECCION 7 OPERADORES </h2><br><br><br>");
          //En esta clase veremos operadores, algo muy sencillo cosa que no me molestare en explicar sino que dejare unos ejemplos.
          
          $var1 = 5;
@@ -131,45 +228,146 @@
          
          //ejemplo de (==) (igual que) este resultado dara false ya que no coinciden.
          if($var1 == $var2){
-          echo "var 1 es igual que var 2";
+          echo "var 1 es igual que var 2<br>  <br>";
 
          }else{
-          echo "Var1 no es igual a Var2";
+          echo "Var1 no es igual a Var2<br>  <br>";
          }
 
          //ejemplo (===) (ideantico a)
          if($var2 === $var3){
-          echo"Las variables comparadas son identicas en su valor";
+          echo"Las variables comparadas son identicas en su valor<br>  <br>";
          }else{
-          echo"Variables no coinciden";
+          echo"Variables no coinciden<br>  <br>";
          }
 
          //ejemplo (!=) (diferente que)
          if($var3 != $var4){
-          echo"Verdadero, estas variables si son diferentes";
+          echo"Verdadero, estas variables si son diferentes<br>  <br>";
          }else{
-          echo "Esta comparacion es falsa las variables no son diferentes";
+          echo "Esta comparacion es falsa las variables no son diferentes<br>  <br>";
          }
 
          //ejemplo (<>) (diferente que y ademas de diferente tipo)
          if($var1 <> $var2){
-          echo "True -> Estas variables son diferentes y de diferente tipo";
+          echo "True -> Estas variables son diferentes y de diferente tipo<br>  <br>";
          }else{
-          echo "False ->";
+          echo "False -><br>  <br>";
          }
 
-         //Faltan ejemplos de (< menor que) (> mayor que) (<= menor igual que) (>= mayor igual que) pero queda claro como se comparan las variables.
-         ?>
+         /*Faltan ejemplos de (< menor que) (> mayor que) (<= menor igual que) (>= mayor igual que) pero queda claro como se comparan las variables.*/
+         
+         /*A continuacion creamos un formulario para que podamos agregar valores (un nombre) y (una edad)
+        */
 
-         <!--A continuacion: Ejercicio donde pondremos todo lo que hemos aprendido por ahora. -->
+        ?>
+         <form action="" method="post" name="datos_usuario" id="datos_usuario">
+         <table>
+           <p><center>Este formulario forma parte de la leccion 7 en la que aplicamos todo lo anterior aprendido</center></p><br>
+           <tr>
+             <td>Nombre:</td>
+             <td><label for="nombre_usuario"></label>
+           <input type="text" name="nombre_usuario" id="nombre_usuario"></td>
+           </tr>
+   
+           <tr>
+             <td>Edad:</td>
+             <td><label for="edad_usuario"></label>
+           <input type="text" name="edad_usuario" id="edad_usuario"></td>
+           </tr>
+           <tr>
+             <td>&nbsp;</td>
+             <td>&nbsp;</td>
+           </tr>
+           <tr>
+             <td colspan="2"><input type="submit" name="enviando" class="boton" id="enviando" value="Enviar"></td>
+           </tr>
+         </table>
+        </form><!--Fin del formulario y empieza nuestro codigo PHP -->
+
          <?php
 
+         //En este ejercicio nos pedia un nombre y una edad, podriamos jugar con los operadores para conseguir diferentes resultados ante el acceso.
+         // ej: En este ejemplo daria igual el nombre pero no la edad, podriamos pedir nombre y edad unicos para el acceso o bien da igual la edad mientras el nombre sea correcto,
+         // a eso nos referimos cuando decimos que podemos jugar con los operadores (== igual a, =! diferente a, < menor que, > mayor que, === extrictamente igual a.)
+         // Usamos IF y ELSE para crear estas condiciones
+
+         if(isset($_POST["enviando"])){
+           $usuario = $_POST["nombre_usuario"];
+           $edad = $_POST["edad_usuario"];
+
+           if($edad > 18 ){
+             echo ("<p class='validado'>Accediendo...</p>");
+        
+            }else{
+             echo ("<p class='no_validado'>Edad invalida</p>");
+           }
+         }
+         //Si queremos enlazar un formlario html a otro documento PHP usaremos
+
+         // ej;  <form action = "validacion.php">   el resultado se vera en una pagina nueva, en el documento php externo podriamos anyadir estilos 
+
+        
+         ?> 
+         
          
 
+         <?php
+         //LECCION 8 // DEFINICION Y DECLARACION DE CONSTANTES
+
+         echo "<h2>LECCION 8 DEFINICION Y DECLARACION DE CONSTANTES</h2>";
+
+         define("AUTOR", "FNF");
+         echo ("este es el resultado guardado dentro de AUTOR : ". AUTOR);
+         //DEFINE = Lo utilizamos para crear CONSTANTES 
+
+         //DEFINE tiene 3 valores, el nombre en string todo mayusculas, El valor y un booleano para 
+         //que pueda diferenciar entre mayusculas y minusculas.
+
+         ?>
+
+         <!--LECCION 9 // OPERADORES 1 (Bastante similares a los de otros lenguajes) --
+
+        1. Empezaremos creando un formulario con todos los operadores que veremos
+
+        2. Nuestra intencion es que podamos hacer operaciones, queremos que al poner dos valores
+           y elegir una opcion /suma/resta..etc. Se envien los datos a nuestro servidor para que sea resuelta
+          la operacion y que cuando acabe no devuelva el resultado en nuestra pagina.-->
 
 
+        <h2>LECCION 9 // OPERADORES 1.</h2>
+
+        <p>&nbsp;</p>
+        
+        <form name="form1" method="post" action="">
+          <p>Creamos un formulario tipo calculadora</p>
+          <p>
+            <label for="num1"></label>
+            <input type="text" name="num1" id="num1">
+            <label for="num2"></label>
+            <input type="text" name="num2" id="num2">
+            <label for="operacion"></label>
+            <select name="operacion" id="operacion" class="boton">
+              <option>Suma</option>
+              <option>Resta</option>
+              <option>Multiplicacion</option>
+              <option>Division</option>
+              <option>Modulo</option>
+            </select>
+          </p>
+          <p>
+            <input type="submit" name="button" class="boton" value="Enviar" onclick="prueba">
+          </p>
+        </form>
+        <p>&nbsp;</p>
+        <!--FIN DEL FORMULARIO COMIENZA NUESTRO CODIGO PHP -->
+        <?php 
+         
+         
          
          ?>
+
+
 
 
 
